9d42bd11eaa2634f9e28eb8b064a10284732ab16
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2024-08-04 02:39:52 -0500

    basic landing page front-end

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,34 @@
-import { login, signup } from "./actions";
+"use client";
+//import { login, signup } from "./actions";
+import { Anchor, Paper, Title, Text, Container, Button } from "@mantine/core";
 
-export default function LoginPage() {
+import { FaMicrosoft } from "react-icons/fa";
+
+export default function Login() {
+  const handleLogin = () => {};
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <Container size={420} my={40}>
+      <Title ta="center" className="font-extrabold">
+        Welcome back!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        Do not have an account yet?{" "}
+        <Anchor size="sm" component="button">
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <Text>Sign in using your Purdue outlook account</Text>
+        <Button
+          fullWidth
+          mt="sm"
+          leftSection={<FaMicrosoft />}
+          onClick={handleLogin}
+        >
+          Sign in to Outlook
+        </Button>
+      </Paper>
+    </Container>
   );
 }
